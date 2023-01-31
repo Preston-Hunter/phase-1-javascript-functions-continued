0cb7ccda070a7b178dcd506267114e134b8d78bb
PrestonHunter <prestonhunter2018@gmail.com>
2023-01-31 10:45:39 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(arg1 = 'roller-skate'){
+
+    return "This Saturday, I want to " + arg1 + "!";
+
+}
+
+function mondayWork(arg1 = 'go to the office'){
+    return "This Monday, I will " + arg1 + ".";
+}
+
+function wrapAdjective(arg1 = '*'){
+
+    return function innter(arg2 = 'special'){
+        return "You are " + arg1 + arg2+ arg1 +"!"; 
+    }
+}
+
